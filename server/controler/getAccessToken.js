11de56d0db513539eceef7f8d1c8e03e2f318b62
ShaunZh <308826842@qq.com>
2018-05-19 22:20:37 +0800

    调整异常处理

--- server/controler/getAccessToken.js
+++ server/controler/getAccessToken.js
@@ -17,36 +17,49 @@ router.post('/', (req, res) => {
       throw new Error('缺少密码');
     }
     const sql = `SELECT ID, USERNAME, PASSWORD FROM WEB_USER WHERE USERNAME = '${username}';`;
+    // 1. 查找出该用户
     database.query(sql, (error, results, fields) => {
-      if (error) {
-        throw new Error(error.message);
-      }
-      if (results.length) {
-        if (password === results[0].PASSWORD) {
-          const token = jwt.sign({
-            iss: results[0].ID,
-            exp: Math.floor(Date.now() + (30 * 24 * 60 * 60 * 1000)) },
-            tokenSecret);
-          console.log(token);
-          const updateSql = `UPDATE WEB_USER SET ACCESS_TOKEN = ? WHERE USERNAME = '${username}';`;
-          const updateParam = [token];
-          database.query(updateSql, updateParam, (error2, results2, fields2) => {
-            if (error2) {
-              throw new Error(error2.message);
-            }
-            res.send(JSON.stringify({
-              status: 200,
-              data: {
-                accessToken: token,
-              },
-              msg: '获取token成功',
-            }));
-          });
-        } else {
+      try {
+        if (error) {
+          throw new Error(error.message);
+        }
+        if (results.length === 0) {
+          throw new Error('没有找到该用户');
+        }
+        if (password !== results[0].PASSWORD) {
           throw new Error('密码错误');
         }
-      } else {
-        throw new Error('没有找到该用户');
+        // 生成token
+        const token = jwt.sign({
+            iss: results[0].ID,
+            exp: Math.floor(Date.now() + (30 * 24 * 60 * 60 * 1000)),
+          },
+          tokenSecret);
+        const updateSql = `UPDATE WEB_USER SET ACCESS_TOKEN = ? WHERE USERNAME = '${username}';`;
+        const updateParam = [token];
+        // 更新token到数据库中
+        database.query(updateSql, updateParam, (error2, results2, fields2) => {
+          // 更新出现错误
+          if (error2) {
+            res.send(JSON.stringify({
+              status: 400,
+              msg: error2.message,
+            }));
+          }
+          // 更新成功
+          res.send(JSON.stringify({
+            status: 200,
+            data: {
+              accessToken: token,
+            },
+            msg: '获取token成功',
+          }));
+        });
+      } catch (e) {
+        res.send(JSON.stringify({
+          status: 400,
+          msg: e.message,
+        }));
       }
     });
   } catch (e) {
