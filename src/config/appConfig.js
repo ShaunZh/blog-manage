324de45fa220ffeb9faa4c174999e5e959e65dd9
ShaunZh <308826842@qq.com>
2018-05-25 01:25:47 +0800

    添加后端访问接口

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -14,6 +14,21 @@ const apiConfig = {
       getAccessToken: `${apiDomain}/getAccessToken`, // 获取访问token
       sendActiveEmail: `${apiDomain}/sendActiveEmail`, // 发送激活链接
     },
+    // 笔记本
+    notebooks: {
+      list: `${apiDomain}/author/notebooks`, // 获取笔记本列表
+      add: `${apiDomain}/author/notebooks`, // 添加笔记本
+      update: `${apiDomain}/author/notebooks`, // 更新笔记本
+      remove: `${apiDomain}/author/notebooks`, // 删除笔记本
+    },
+    // 笔记
+    notes: {
+      list: `${apiDomain}/author/notes`, // 获取文章列表 get方法
+      get: `${apiDomain}/author/notes`, // 获取文章详情 get方法 + 文章ID
+      add: `${apiDomain}/author/notes`, // 添加文章 post方法
+      autoSave: `${apiDomain}/author/notes/autoSave`, // 自动保存文章
+      update: `${apiDomain}/author/notes`, // 更新文章，put方法
+    },
   },
 };
 
